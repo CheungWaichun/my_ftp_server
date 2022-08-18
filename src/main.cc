3e9dcbd7e3f13b68bbbc6298061825a744b179af
zhangweijun <zhangweijun@scutech.com>
2022-08-18 23:54:09 +0800

    ip and port should be specified when starting the server.

--- src/main.cc
+++ src/main.cc
@@ -1,19 +1,15 @@
 #include "../include/Acceptor.h"
 #include <iostream>
-#include <cstdlib>
+// #include <cstdlib>
 // using namespace std;
 int main(int argc, char* argv[]){
-    std::cout<<"hi"<<std::endl;
-    // std::printf("hi");
-    ACE_INET_Addr addr(8888);
+    if(argc < 3){
+        ACE_OS::exit(1);
+    }
+    ACE_INET_Addr addr(((std::string)argv[1] + ":" + argv[2]).c_str());
     Acceptor *eh = new Acceptor(addr);
-    // printf("hi");
-    std::cout<<"hi"<<std::endl;
 
     ACE_Reactor::instance()->register_handler(eh, ACE_Event_Handler::ACCEPT_MASK);
-    printf("hi");
-    std::cout<<"hi"<<std::endl;
-
     while(1)
         ACE_Reactor::instance()->handle_events();
 }
